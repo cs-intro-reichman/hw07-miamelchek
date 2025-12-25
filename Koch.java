838290830aaedd7c9a42550e77a268ca5be8ffed
Mia Melchek <miamelchek@Mias-MacBook-Air.local>
2025-12-25 13:21:23 +0200

    HW7 Koch fractal final version

--- Koch.java
+++ Koch.java
@@ -8,35 +8,56 @@ public class Koch {
 		//// Uncomment only one block in each test, and remember to compile
 		//// the class whenever you change the test.
 
-        /*
+        /* 
 		// Tests the curve function:
 		// Gets n, x1, y1, x2, y2,
 		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
 		curve(Integer.parseInt(args[0]),
 			  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
 		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
-		*/
-
-		/*
+ 		*/
+		
 		// Tests the snowflake function:
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
 		snowFlake(Integer.parseInt(args[0]));
-		*/
+		
 	}
 
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		if (n == 0) {
+   			StdDraw.line(x1, y1, x2, y2);
+    		return;
+		}
+
+		double ax = x1 + (x2 - x1) / 3.0;
+		double ay = y1 + (y2 - y1) / 3.0;
+		double bx = x1 + 2*(x2 - x1) / 3.0;
+		double by = y1 + 2*(y2 - y1) / 3.0;
+		double cx = 0.5 * (x1 + x2) - (Math.sqrt(3) / 6.0) * (y2 - y1);
+		double cy = (Math.sqrt(3) / 6.0) * (x2 - x1) + 0.5 * (y1 + y2);
+
+
+
+		curve(n - 1, x1, y1, ax, ay);   // P -> A
+		curve(n - 1, ax, ay, cx, cy);   // A -> C
+		curve(n - 1, cx, cy, bx, by);   // C -> B
+		curve(n - 1, bx, by, x2, y2);   // B -> Q	
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
 	public static void snowFlake(int n) {
 		// A little tweak that makes the drawing look better
-		StdDraw.setYscale(0, 1.1);
-		StdDraw.setXscale(0, 1.1);
-		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		StdDraw.setXscale(-0.2, 1.2); 
+    	StdDraw.setYscale(-0.2, 1.2);
+		double ax = 0.1, ay = 0.1;
+		double bx = 1.0, by = 0.1;
+		double cx = 0.55, cy = 0.1 + 0.9 * 0.866;
+
+		curve(n, ax, ay, cx, cy);
+    	curve(n, cx, cy, bx, by); 
+    	curve(n, bx, by, ax, ay);
+
 	}
 }
--- Sierpinski.java
+++ Sierpinski.java
@@ -3,16 +3,42 @@ public class Sierpinski {
 	
 	public static void main(String[] args) {
 		sierpinski(Integer.parseInt(args[0]));
+
+
 	}
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+		// setting the canvas
+		StdDraw.setCanvasSize(600, 600);
+		StdDraw.setXscale(0.0, 1.0);
+		StdDraw.setYscale(0.0, 1.0);
+		StdDraw.clear();
+		
+		//bulding the triangle
+		double x1 = 0.0, y1 = 0.0;
+        double x2 = 1.0, y2 = 0.0;
+        double x3 = 0.5, y3 = 0.866;
+
+        sierpinski(n, x1, x2, x3, y1, y2, y3);
 	}
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+		if (n == 0) {
+			StdDraw.line(x1, y1, x2, y2);
+        	StdDraw.line(x2, y2, x3, y3);
+        	StdDraw.line(x3, y3, x1, y1);
+        	return;
+			
+		}
+		double mx12 = (x1 + x2) / 2.0, my12 = (y1 + y2) / 2.0;
+    	double mx23 = (x2 + x3) / 2.0, my23 = (y2 + y3) / 2.0;
+    	double mx31 = (x3 + x1) / 2.0, my31 = (y3 + y1) / 2.0; 
+
+    	sierpinski(n - 1, x1,  mx12, mx31, y1,  my12, my31);
+    	sierpinski(n - 1, mx12, x2,  mx23, my12, y2,  my23);
+    	sierpinski(n - 1, mx31, mx23, x3,  my31, my23, y3);
 	}
 }
